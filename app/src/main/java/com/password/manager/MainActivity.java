19b6b49b959488e2abc62a4e603424290e404d24
Clemens Gerstung <clemens8496@gmail.com>
2014-11-27 20:06:39 +0100

    added MainActivity and main_layout changed Design to Material-Design

---
+++ app/src/main/java/com/password/manager/MainActivity.java
@@ -0,0 +1,32 @@
+package com.password.manager;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.activity_main_menu_change_password) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
